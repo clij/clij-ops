d6a1ad37c35c3bef39d2b8200d2f2a226eb54655
haesleinhuepf <rhaase@mpi-cbg.de>
2018-12-30 11:57:18 +0100

    added documentation, updated parent pom version and create a menu entry

--- src/main/java/com/yourdomain/clijplugin/PluginTemplate.java
+++ src/main/java/com/yourdomain/clijplugin/PluginTemplate.java
@@ -12,6 +12,10 @@ import java.util.HashMap;
 /**
  * The PluginTemplate serves as starting point for making your own plugin.
  *
+ * The name of the class and the name of your plugin method call must be as similar as shown here.
+ * Assuming the class is called "PluginTemplate", the method name must be "CLIJ_pluginTemplate". Otherwise, it may not
+ * be found be the macro interpreter.
+ *
  * Enter the correct name of your method here: -------------
  *                                                         |
  *                                                         |
@@ -24,28 +28,44 @@ public class PluginTemplate extends AbstractCLIJPlugin implements CLIJMacroPlugi
     @Override
     public boolean executeCL() {
         Object[] args = openCLBufferArgs();
+        // -------------------------------------------------------------------------------------------------------------
+        // todo: Build in your own code here. The variable args contains all parameters handed over in the order as
+        //      entered in ImageJ macro or in the dialog. Images have type ClearCLBuffer, numbers come as Double.
+        //      Use the methods asFloat(args[n]), asInteger(args[n]), asBoolean(args[n]) to convert them properly.
+
         boolean result = addScalar((ClearCLBuffer)( args[0]), (ClearCLBuffer)(args[1]), asFloat(args[2]));
+
+        // -------------------------------------------------------------------------------------------------------------
         releaseBuffers(args);
         return result;
     }
 
+    // -----------------------------------------------------------------------------------------------------------------
+    // todo: enter your corde here. The argument order may be changed. However, it is recommended to use the same order
+    //       as defined in the getParameterHelpText() method to prevent confusion.
     private boolean addScalar(ClearCLBuffer src, ClearCLBuffer dst, Float scalar) {
         HashMap<String, Object> lParameters = new HashMap<>();
         lParameters.put("src", src);
         lParameters.put("scalar", scalar);
         lParameters.put("dst", dst);
 
-        return clij.execute(PluginTemplate.class,
-                "template.cl",
-                "addScalar_" + src.getDimension() + "d",
-                lParameters);
+        // todo: The location of the .cl file must be relative to the class specified below:
+        return clij.execute(PluginTemplate.class, "template.cl", "addScalar_" + src.getDimension() + "d", lParameters);
     }
 
+    // -----------------------------------------------------------------------------------------------------------------
+    // todo: enter the list of parameters you need in order to make your algorithm run properly. Use the words
+    //       Image, Number, String, Boolean to specify the right types.
+    //       Name your output images with a name containing "destination" to make CLIJ generate them automatically and
+    //       not prompt the user for it.
     @Override
     public String getParameterHelpText() {
         return "Image source, Image destination, Number scalar";
     }
 
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // todo: enter the documentation for your plugin here:
     @Override
     public String getDescription() {
         return "Detailed description of your plugin.\n" +
@@ -53,6 +73,8 @@ public class PluginTemplate extends AbstractCLIJPlugin implements CLIJMacroPlugi
                 "What do the parameters mean?";
     }
 
+    // -----------------------------------------------------------------------------------------------------------------
+    // todo: enter the image dimensionality for which your algorithm might be applied:
     @Override
     public String getAvailableForDimensions() {
         return "2D, 3D";
--- src/main/java/com/yourdomain/clijplugin/template.cl
+++ src/main/java/com/yourdomain/clijplugin/template.cl
@@ -1,3 +1,13 @@
+// todo: Enter your OpenCL code here, rename the .cl file or generate an own .cl file.
+//
+// Use the method place holders READ_IMAGE_2D and READ_IMAGE_3D to read from an input image.
+// Use the method place holders WRITE_IMAGE_2D and WRITE_IMAGE_3D to write to an ouput image.
+// Mark your images as input images using the type place holders DTYPE_IMAGE_IN_2D and DTYPE_IMAGE_IN_3D.
+// Mark your images as output images using the type place holders DTYPE_IMAGE_OUT_2D and DTYPE_IMAGE_OUT_3D.
+// Pixel types of input and output images are specified with DTYPE_IN and DTYPE_OUT.
+// To read the image size for within an OpenCL method, use the method place holders GET_IMAGE_IN_WIDTH,
+// GET_IMAGE_IN_HEIGHT and GET_IMAGE_IN_DEPTH.
+//
 __constant sampler_t sampler = CLK_NORMALIZED_COORDS_FALSE | CLK_ADDRESS_CLAMP_TO_EDGE | CLK_FILTER_NEAREST;
 
 __kernel void addScalar_3d(DTYPE_IMAGE_IN_3D  src,
